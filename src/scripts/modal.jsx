b48ea679836a9b289a2277298620a51f48e8431f
Amilcar J. Infante <amilcar.infante@globant.com>
2015-04-29 10:11:41 -0300

    Create modal Component

---
+++ src/scripts/modal.jsx
@@ -0,0 +1,26 @@
+var React = require('react/addons'),
+    ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
+
+require('modal.css');
+
+module.exports = React.createClass({
+    propTypes: {
+        display: React.PropTypes.bool.isRequired
+    },
+    render: function() {
+        var modal;
+        if( this.props.display ){
+            modal = <div className='react-modal'>
+                        <div className="modal-dialog">
+                            {this.props.children}
+                        </div>
+                    </div>;
+        }
+        return (
+            <ReactCSSTransitionGroup
+                transitionName="modalTransition">
+                    {modal}
+            </ReactCSSTransitionGroup>
+        )
+    }
+});
